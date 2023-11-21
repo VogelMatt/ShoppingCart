0b6e7dd9189ab0f20028db042a5a8dfb64960e29
ToastyandTasty <mahvogel@gmail.com>
2023-11-21 00:41:25 -0600

    photos imported and cards staged

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,18 @@ import { About } from "./pages/About";
 import { Navbar } from "./components/Navbar";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Container className="mb-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<Container className="mb-4">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/store" element={<Store />} />
+					<Route path="/about" element={<About />} />
+				</Routes>
+			</Container>
+		</>
+	);
 }
 
 export default App;
