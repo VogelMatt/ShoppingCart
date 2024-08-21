69a22236b69cf11ba07bd2a8c60d2674a1577620
ToastyandTasty <mahvogel@gmail.com>
2024-08-20 23:59:22 -0500

    price structure

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import storeItems from "../data/items.json";
+import { formatCurrency } from "../utilities/formatCurrency";
 
 type CartItemProps = {
 	id: number;
@@ -27,7 +28,11 @@ export function CartItem({ id, quantity }: CartItemProps) {
 						</span>
 					)}
 				</div>
+				<div className="text-muted" style={{fontSize: ".75rem"}}>
+					{formatCurrency(item.price)}
+				</div>
 			</div>
+			<div>{formatCurrency(item.price * quantity)}</div>
 		</Stack>
 	);
 }
